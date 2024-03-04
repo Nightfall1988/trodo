0c2e13dc67963ad47a2786b78f8d693bf388beb3
grigorijs <Nightfall1988@github.com>
2024-03-04 02:34:03 +0200

    debugged

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 require('./bootstrap');
 import {createApp} from 'vue'
-
+import { Bootstrap4Pagination } from 'laravel-vue-pagination';
+import { Bootstrap5Pagination } from 'laravel-vue-pagination';
+import { TailwindPagination } from 'laravel-vue-pagination';
 import CurrencyComponent from './components/CurrencyComponent.vue'
 
 createApp(CurrencyComponent).mount("#app")
